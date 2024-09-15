f27622101a46c891ac55319e899b21395bf96a5d
Jacob Tran <jacobntran@gmail.com>
2024-09-15 10:52:57 -0700

    created s3 bucket for file storage and permissions for the app instance to upload/download

--- main.tf
+++ main.tf
@@ -13,14 +13,19 @@ variable "key_name" {
   type        = string
 }
 
+variable "bucket_name" {
+  description = "Name of the bucket where you'll store your files"
+  type        = string
+}
+
 output "bastion_host_public_ip" {
-    value = aws_instance.bastion.public_ip
-    description = "The public IP of the Bastion Host"
+  value       = aws_instance.bastion.public_ip
+  description = "The public IP of the Bastion Host"
 }
 
 output "app_host_private_ip" {
-    value = aws_instance.app.private_ip
-    description = "The private IP of the App Host"
+  value       = aws_instance.app.private_ip
+  description = "The private IP of the App Host"
 }
 
 ### NETWORKING ###
@@ -117,6 +122,7 @@ resource "aws_instance" "app" {
   subnet_id              = aws_subnet.private.id
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.app.id]
+  iam_instance_profile   = aws_iam_instance_profile.s3_file_uploader.name
 
   tags = {
     Name = "S3 File Uploader Host"
@@ -161,4 +167,97 @@ resource "aws_security_group" "app" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+### STORAGE ###
+resource "aws_s3_bucket" "this" {
+  bucket        = var.bucket_name
+  force_destroy = true ## change to false if you don't want terraform to destroy your objects when running terraform destroy
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_kms_key" "s3" {
+  description = "Key for encrypting S3 objects"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "kms" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3.id
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+### PERMISSIONS ###
+resource "aws_iam_policy" "s3_file_uploader" {
+  name        = "s3_file_uploader"
+  description = "Used by S3 file uploader application to upload and download objects to your S3 bucket"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ],
+        "Resource" : [
+          "${aws_s3_bucket.this.arn}",
+          "${aws_s3_bucket.this.arn}/*"
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
+        "Resource" : "${aws_kms_key.s3.arn}"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "s3_file_uploader" {
+  name = "ec2_instance_s3_file_uploader_role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.s3_file_uploader.name
+  policy_arn = aws_iam_policy.s3_file_uploader.arn
+}
+
+resource "aws_iam_instance_profile" "s3_file_uploader" {
+  name = "ec2_instance_s3_file_uploader_instance_profile"
+  role = aws_iam_role.s3_file_uploader.name
 }
