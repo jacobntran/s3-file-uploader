69cbb8e6b6c41dd774a89640ef280f310c503c6a
Jacob Tran <jacobntran@gmail.com>
2024-09-15 08:48:54 -0700

    adding code for initial networking and services

--- main.tf
+++ main.tf
@@ -1,3 +1,164 @@
 provider "aws" {
-    region = "us-west-1"
+  region = "us-west-1"
+}
+
+### VARIABLES ###
+variable "my_ip" {
+  description = "Your public ip address"
+  type        = string
+}
+
+variable "key_name" {
+  description = "The key used to access your instances"
+  type        = string
+}
+
+output "bastion_host_public_ip" {
+    value = aws_instance.bastion.public_ip
+    description = "The public IP of the Bastion Host"
+}
+
+output "app_host_private_ip" {
+    value = aws_instance.app.private_ip
+    description = "The private IP of the App Host"
+}
+
+### NETWORKING ###
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/27"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.0.0/28"
+  availability_zone       = "us-west-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = "10.0.0.16/28"
+  availability_zone = "us-west-1b"
+
+  tags = {
+    Name = "Private Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_eip" "nat_gw" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "public" {
+  allocation_id = aws_eip.nat_gw.id
+  subnet_id     = aws_subnet.public.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "Public Subnet Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.public.id
+  }
+
+  tags = {
+    Name = "Private Subnet Route Table"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
+### SERVICES ###
+resource "aws_instance" "bastion" {
+  ami                    = "ami-0d53d72369335a9d6"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.bastion.id]
+
+  tags = {
+    Name = "Bastion Host"
+  }
+}
+
+resource "aws_instance" "app" {
+  ami                    = "ami-0d53d72369335a9d6"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.private.id
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.app.id]
+
+  tags = {
+    Name = "S3 File Uploader Host"
+  }
+}
+
+resource "aws_security_group" "bastion" {
+  name        = "bastion_host"
+  description = "Bastion Host Security Group"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "app" {
+  name        = "app_host"
+  description = "Application Host Security Group"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/28"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
